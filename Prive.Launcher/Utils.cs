740185974d31be4b9b138bb3fe60b6733721b2cf
pdf114514 <haruton321@outlook.jp>
2023-05-16 15:15:26 +0900

    Inject Dll

--- Prive.Launcher/Utils.cs
+++ Prive.Launcher/Utils.cs
@@ -1,53 +1,11 @@
-using System.Runtime.InteropServices;
 using ConsoleGUI.Space;
 using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Prive.Launcher {
     public static class Utils {
-        /* public static void Resize(in Size size) {
-            Console.SetWindowSize(size.Width, size.Height);
-            Console.Write($"\x1b[8;{size.Height};{size.Width}t");
-
-            IntPtr handle = GetConsoleWindow();
-
-            if (handle != IntPtr.Zero) {
-                SetConsoleWindowInfo(handle, true, new SmallRect() {
-                    Left = 0,
-                    Top = 0,
-                    Right = (short)size.Width,
-                    Bottom = (short)size.Height
-                });
-            }
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct SmallRect {
-            public short Left;
-            public short Top;
-            public short Right;
-            public short Bottom;
-        }
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool SetConsoleWindowInfo(IntPtr hConsoleOutput, bool bAbsolute, in SmallRect lpConsoleWindow);
-
-        public static uint ForceV2 { get => GetForceV2(); set => SetForceV2(value); }
-
-        public static uint GetForceV2() {
-            var key = Registry.CurrentUser.OpenSubKey("Console", true);
-            if (key is null) return (uint)0;
-            var result = uint.Parse((string)key.GetValue("ForceV2"));
-            key.Close();
-            return result;
-        }
-
-        public static void SetForceV2(uint value) {
-            var key = Registry.CurrentUser.OpenSubKey("Console", true);
-            if (key is null) return;
-            key.SetValue("ForceV2", value, RegistryValueKind.DWord);
-            key.Close();
-        } */
-
         public const int MF_BYCOMMAND = 0x00000000;
         public const int SC_CLOSE = 0xF060;
         public const int SC_MINIMIZE = 0xF020;
@@ -68,6 +26,50 @@ namespace Prive.Launcher {
             IntPtr sysMenu = GetSystemMenu(handle, false);
 
             if (handle != IntPtr.Zero) DeleteMenu(sysMenu, nPosition, MF_BYCOMMAND);
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 SuspendThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern int ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenThread(int dwDesiredAccess, bool bInheritHandle, int dwThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hHandle);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        public static void InjectDll(Process process, string filePath) => InjectDll(process.Id, filePath);
+
+        public static void InjectDll(int processId, string filePath) {
+            if (!File.Exists(filePath)) throw new FileNotFoundException("File not found", filePath);
+
+            var handle = OpenProcess(0x1F0FFF, false, processId);
+            var loadLibrary = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+            var size = (uint)((filePath.Length + 1) * Marshal.SizeOf(typeof(char)));
+            var address = VirtualAllocEx(handle, IntPtr.Zero, size, 0x1000 | 0x2000, 4);
+            
+            WriteProcessMemory(handle, address, Encoding.Default.GetBytes(filePath), size, out _);
+            CreateRemoteThread(handle, IntPtr.Zero, 0, loadLibrary, address, 0, IntPtr.Zero);
         }
     }
 }
