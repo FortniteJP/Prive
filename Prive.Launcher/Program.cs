0047f964ac79acc7e615642d5f0f5ebea8f3861c
pdf114514 <haruton321@outlook.jp>
2023-05-16 11:38:53 +0900

    Linux

--- Prive.Launcher/Program.cs
+++ Prive.Launcher/Program.cs
@@ -1,38 +1,64 @@
-﻿using ConsoleGUI;
+using ConsoleGUI;
 using ConsoleGUI.Api;
 using ConsoleGUI.Controls;
 using ConsoleGUI.Space;
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Prive.Launcher {
     public class Program {
+        public static readonly bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        public static readonly bool IsServer = !IsWindows; // Run server on Linux
+        public static readonly int RefreshIntervalMS = IsWindows ? 10 : 100;
+        public static bool Running { get; set; } = true;
+
         public static void Main(string[] args) {
-            if (!args.Contains("/conhost")) {
+            if (IsWindows &&  !args.Contains("/conhost")) {
                 // To avoid running in Windows Terminal, because ui is broken there
                 Process.Start("conhost.exe", $"\"{Assembly.GetEntryAssembly()?.Location.Replace(".dll", ".exe")}\" /conhost");
                 Environment.Exit(0);
             }
 
-            Utils.Delete(Utils.SC_SIZE);
-            Utils.Delete(Utils.SC_MAXIMIZE);
             Console.Title = "Prive";
+            if (IsWindows) {
+                Utils.Delete(Utils.SC_SIZE);
+                Utils.Delete(Utils.SC_MAXIMIZE);
+                ConsoleManager.Resize(new(25, 5));
+            }
             // Set Icon? https://stackoverflow.com/a/59897483
             ConsoleManager.Console = new SimplifiedConsole();
-            ConsoleManager.Resize(new(25, 5));
             ConsoleManager.Setup();
-            MouseHandler.Initialize();
+            if (IsWindows) MouseHandler.Initialize();
             
-            ConsoleManager.Content = new Box() { 
+            MainUI();
+        }
+
+        public static void MainUI() {
+            var canvas = new Canvas();
+
+            // How to move this center
+            canvas.Add(
+                new Background() { Color = new(10, 40, 10), Content = new Border() { Content = new Box() { Content = new TextBlock() { Text = "Prive Launcher!" } } } }, new(0, 7, 17, 5)
+            );
+
+            /* ConsoleManager.Content = new Box() { 
                 HorizontalContentPlacement = Box.HorizontalPlacement.Center,
                 VerticalContentPlacement = Box.VerticalPlacement.Center,
-                Content = new TextBlock() { Text = "Prive Launcher!" }
-            };
+                Content = canvas
+            }; */
+            ConsoleManager.Content = canvas;
             
-            while (true) {
-                Thread.Sleep(10);
+            while (Running) {
+                Thread.Sleep(RefreshIntervalMS);
                 ConsoleManager.AdjustBufferSize(); // Handle resizing
             }
+        }
+
+        public static async void ObserveInAsync() {
+            await Task.Delay(1);
+            await Console.In.ReadLineAsync();
+            Running = false;
         }
     }
 }
--- Prive.Launcher/Utils.cs
+++ Prive.Launcher/Utils.cs
@@ -4,7 +4,7 @@ using Microsoft.Win32;
 
 namespace Prive.Launcher {
     public static class Utils {
-        public static void Resize(in Size size) {
+        /* public static void Resize(in Size size) {
             Console.SetWindowSize(size.Width, size.Height);
             Console.Write($"\x1b[8;{size.Height};{size.Width}t");
 
@@ -31,9 +31,6 @@ namespace Prive.Launcher {
         [DllImport("kernel32.dll", SetLastError = true)]
         private static extern bool SetConsoleWindowInfo(IntPtr hConsoleOutput, bool bAbsolute, in SmallRect lpConsoleWindow);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern IntPtr GetConsoleWindow();
-
         public static uint ForceV2 { get => GetForceV2(); set => SetForceV2(value); }
 
         public static uint GetForceV2() {
@@ -49,13 +46,16 @@ namespace Prive.Launcher {
             if (key is null) return;
             key.SetValue("ForceV2", value, RegistryValueKind.DWord);
             key.Close();
-        }
+        } */
 
         public const int MF_BYCOMMAND = 0x00000000;
         public const int SC_CLOSE = 0xF060;
         public const int SC_MINIMIZE = 0xF020;
         public const int SC_MAXIMIZE = 0xF030;
         public const int SC_SIZE = 0xF000;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetConsoleWindow();
 
         [DllImport("user32.dll")]
         public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
