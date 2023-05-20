6f949f2251568de311d5526a3b6592782f8c3a1f
pdf114514 <haruton321@outlook.jp>
2023-05-20 20:01:48 +0900

    Fix

--- Prive.Native.Client/dllmain.cpp
+++ Prive.Native.Client/dllmain.cpp
@@ -8,11 +8,18 @@ void Main() {
         freopen_s(&pFile, "CONOUT$", "w", stdout);
     }
 
-    auto easy_find = FindPattern(
+    auto easyFind = FindPattern(
         "\x89\x54\x24\x10\x4C\x89\x44\x24\x18\x4C\x89\x4C\x24\x20\x48\x83\xEC\x28\x48\x85\xC9\x75\x08\x8D\x41\x2B\x48\x83\xC4\x28\xC3\x4C",
         0x20
     );
-    OCurlEasySetOpt = (decltype(OCurlEasySetOpt))easy_find;
+    auto setFind = FindPattern(
+        "\x48\x89\x5C\x24\x08\x48\x89\x6C\x24\x10\x48\x89\x74\x24\x18\x57\x48\x83\xEC\x30\x33\xED\x49\x8B\xF0\x48\x8B\xD9",
+        0x1c
+    );
+
+    OCurlEasySetOpt = (decltype(OCurlEasySetOpt))easyFind;
+    OCurlSetOpt = (decltype(OCurlSetOpt))setFind;
+
     EnableHook(OCurlEasySetOpt, CurlEasySetOptDetour);
 }
 
